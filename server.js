4fd17dfc0f79e74fa761cc2a8f4ec1113ed5dfa6
Trevor Johnson <trevor9j@protonmail.com>
2024-03-31 22:20:26 -0700

    created databases, added table information, established connection, and server.js with prompts and functionality

--- server.js
+++ server.js
@@ -1,8 +1,54 @@
 const inquirer = require('inquirer');
-const my sql = require('./config/connection');
+const db = require('./config/connection');
+const utils = require("util");
+db.query = utils.promisify(db.query);
 
-const addDepartmentQuestions [
-  {
-    type
-  }
-]
+function startApp() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Quit",
+        ],
+
+      },
+    ])
+
+    .then((options) => {
+      switch (options.choice) {
+        case "View all departments":
+          viewDepts();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRoleAA();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployee();
+          break;
+        case "Quit":
+          db.end();
+      }
+    });
+}
